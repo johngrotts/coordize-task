26eab6fdaef9fb5772b62c5105917d049ba78be2
johngrotts <johngrotts@users.noreply.github.com>
2023-09-17 21:37:29 -0500

    Initial Spring commit

---
+++ src/main/java/com/coordize/coordizetask/CoordizeTaskApplication.java
@@ -0,0 +1,13 @@
+package com.coordize.coordizetask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoordizeTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoordizeTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coordize/coordizetask/CoordizeTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coordize.coordizetask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoordizeTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
